6342109d24fe0d9cd445d0248ed1d60212a807ae
Nadeeshi-pd <nadeeshiprabodima@gmail.com>
2025-05-30 17:06:54 +0530

    Initial commit on Nadeeshi branch

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function WelcomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome</Text>
+      <Text style={styles.text}>Welcome here</Text>
       
     </View>
   );
