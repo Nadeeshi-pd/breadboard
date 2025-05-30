5c5b6d363937b7ea27c4a5fa8d5a2742a00b4fb4
Nadeeshi-pd <nadeeshiprabodima@gmail.com>
2025-05-30 17:38:55 +0530

    change bg color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function WelcomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Welcome here</Text>
+      <Text style={styles.text}>Welcome here bye</Text>
       
     </View>
   );
